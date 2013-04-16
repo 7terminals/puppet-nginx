06c00ccf5fa85c037ac311e5ee1cb674d628a2cf
jude <judebpereira@gmail.com>
2013-04-16 19:40:09 +0530

    testing done

--- tests/init.pp
+++ tests/init.pp
@@ -1,13 +1,4 @@
 nginx::setup { 'webserver':
-  #  ensure     => [
-  #    {
-  #      "module" => "present"
-  #    }
-  #    ,
-  #    {
-  #      "service" => "running"
-  #    }
-  #    ],
   ensure     => 'running',
   enable     => true,
   autoupdate => false
